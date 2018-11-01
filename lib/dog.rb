7972698ebbddfc25664c6248ff126b62fed19146
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-11-01 04:03:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,6 +47,12 @@ class Dog
     dog
   end
   
+  def self.find_by_id(id)
+    sql = "SELECT * FROM dogs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+  
   def self.new_from_db(row)
     dog = Dog.new(name: row[1], breed: row[2], id: row[0])
     dog
